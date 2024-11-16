3be60f413823bfd61ef4ce01161babfddc8d349f
Alifseen <alifseen@gmail.com>
2024-11-16 10:14:49 +0500

    Created Context example to display static text on html using jinja

--- food_management/views.py
+++ food_management/views.py
@@ -8,6 +8,11 @@ class MealsList(generic.ListView):
     queryset = Meals.objects.order_by("-date_created")  ## Connects values in model database to the view
     template_name = "index.html"  ## connects view to html
 
+    ## Usinng context manager and jinja to get data on html
+    def get_context_data(self): ## Use this exact method name to overwrite the Listviews original method
+        context = {"meals":"Pizza", "Ingredients":["Pizza Bread", "Pizza Sauce"]}  ## Context needs to be a dictionary
+        return context
+
 
 class MealDetail(generic.DetailView):
     model = Meals
