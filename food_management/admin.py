d1ce630247908c3dcdb88c70258347a52eca6fb5
Alifseen <alifseen@gmail.com>
2024-11-16 11:18:41 +0500

    Created an Admin panel, super user and added meals to the database

--- food_management/admin.py
+++ food_management/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from.models import Meals
 
-# Register your models here.
+class MenuItemAdmin(admin.ModelAdmin):
+    list_display = ("dish_name", "dish_category", "availability")
+    list_filter = ("availability", )
+    search_fields = ("dish_name", "ingredients")
+
+admin.site.register(Meals, MenuItemAdmin)
