4d31ed3dbb37271add91722cfdbd728a0067445c
Alifseen <alifseen@gmail.com>
2024-11-16 10:04:05 +0500

    Created view classes and html files, connected them as well as the model db

--- food_management/views.py
+++ food_management/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Meals
 
-# Create your views here.
+
+## Display menu as a list view class
+class MealsList(generic.ListView):
+    queryset = Meals.objects.order_by("-date_created")  ## Connects values in model database to the view
+    template_name = "index.html"  ## connects view to html
+
+
+class MealDetail(generic.DetailView):
+    model = Meals
+    template_name = "meal_detail.html"
