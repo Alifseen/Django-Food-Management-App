3efcc38300ad92276eb2a879d28ec22d638b83ac
Alifseen <alifseen@gmail.com>
2024-11-16 11:44:04 +0500

    Added dynamic links using primary keys in urls.py and jinja

--- food_management/urls.py
+++ food_management/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path("", views.MealsList.as_view(), name="index"),
-    path("details", views.MealDetail.as_view(), name="meal_detail")
+    path("details/<int:pk>", views.MealDetail.as_view(), name="meal_detail")  ## added a dynamic placed for primarkey for whatever item is clicked
 ]
